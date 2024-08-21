73b94770f63470f8a47d774930f4d70141cabe35
mererng <flivino0@gmail.com>
2024-08-20 22:35:48 -0300

    first commit

---
+++ base.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+//Função Principal:
+int main(){
+
+    setlocale(LC_ALL,"Portuguese_Brazil");
+
+    printf("***********************\n");
+    printf("//JOGO DE ADIVINHAÇÃO//\n");
+    printf("***********************\n");
+    
+    int secreto = 52;
+
+    int chute;
+
+    printf("Bem Vindo ao jogo onde seu objetivo seré acertar o número secreto!\n");
+    printf("Qual é seu Chute?: ");
+    scanf("%d" , &chute);//Colocar o "&" antes da variavel � importante.
+    printf("Seu chute foi %d.\n", chute);
+    
+    if(secreto == chute){
+        printf("Parabens, Voc� acertou!");
+    }
+    else {
+
+        int chuteMenor = chute < secreto;
+        if(chuteMenor){
+            printf("O número é maior.\n");
+        }
+        else{
+            printf("O núumero é menor.\n");
+        }
+
+        printf("Você errou, tente novamente!");
+    }
+
+    return 0;
+}
